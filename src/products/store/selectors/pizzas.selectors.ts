fb79eb8baea43128ef2fa38753edf54e37f34d1c
Jithin Mathew <Jithin@Jithins-MacBook-Pro.local>
2018-03-23 18:33:44 +0530

    done upto router state composition

---
+++ src/products/store/selectors/pizzas.selectors.ts
@@ -0,0 +1,41 @@
+import { ActionReducerMap, createSelector, createFeatureSelector} from '@ngrx/store';
+
+import * as fromRoot from '../../../app/store';
+import * as fromFeatures from '../reducers';
+import * as fromPizzas from '../reducers/pizzas.reducer';
+
+
+import { Pizza } from '../../models/pizza.model';
+
+
+
+// pizzas state
+export const getPizzaState = createSelector(
+  fromFeatures.getProductsState,
+  (state: fromFeatures.ProductsState) => state.pizzas
+);
+
+
+export const getPizzasEntities = createSelector(
+  getPizzaState,
+  fromPizzas.getPizzasEntities
+);
+
+export const getSelectedPizza = createSelector(
+  getPizzasEntities,
+  fromRoot.getRouterState,
+  (entities, router): Pizza => {
+    return router.state && entities[router.state.params.pizzaId];
+  }
+);
+
+export const getAllPizzas = createSelector(
+  getPizzasEntities,
+  (entities) => {
+    return Object.keys(entities).map(id => entities[parseInt(id, 10)]);
+  }
+);
+
+
+export const getPizzasLoaded = createSelector(getPizzaState, fromPizzas.getPizzasLoaded);
+export const getPizzasLoading = createSelector(getPizzaState, fromPizzas.getPizzasLoading);
